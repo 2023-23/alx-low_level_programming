b40a8cab94b6f9c0aecd29c07e9876f9174c59a6
2023-23 <meryem.ag99@gmail.com>
2023-03-21 15:15:22 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - prints lowercase alphabet
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
