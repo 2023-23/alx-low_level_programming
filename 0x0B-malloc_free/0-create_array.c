81e4ddc56777724616f02dd06f866eb8b722944a
2023-23 <meryem.ag99@gmail.com>
2023-04-11 18:24:16 -0700

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,10 +11,10 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *str;
+	char * str;
 	unsigned int i;
 
-	str = malloc(sizeof((char) *size);
+	str = malloc(sizeof(char) * size);
 	if (size == 0 || str == NULL)
 		return (NULL);
 
